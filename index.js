4c56941d04576be0e178e25643577db67b021017
Jannis R <mail@jannisr.de>
2016-12-02 12:55:29 +0100

    add & remove dots

--- index.js
+++ index.js
@@ -6,16 +6,27 @@ const randomColor = require('random-color')
 
 // -------------------------------
 
-const renderDot = (dot) => {
+const renderDot = (dot, i, removeDot) => {
+	const onClick = () => removeDot(i)
 	return yo `
-		<circle cx="${dot.x}" cy="${dot.y}" r="${dot.r}" fill="${dot.color}" />
+		<circle
+			cx="${dot.x}" cy="${dot.y}"
+			r="${dot.r}"
+			fill="${dot.color}"
+			onclick=${onClick}
+		/>
 	`
 }
 
-const render = (width, height, dots) => {
+const render = (width, height, dots, removeDot, addDot) => {
 	return yo `
 		<svg width="${width}" height="${height}">
-			${dots.map(renderDot)}
+			<rect
+				width="${width}" height="${height}"
+				fill="transparent"
+				onclick=${addDot}
+			/>
+			${dots.map((dot, i) => renderDot(dot, i, removeDot))}
 		</svg
 	`
 }
@@ -25,6 +36,11 @@ const render = (width, height, dots) => {
 const width = 400
 const height = 400
 let dots = []
+
+const removeDot = (i) => {
+	dots = dots.filter((dot, j) => j !== i)
+	rerender()
+}
 
 const addDot = () => {
 	dots = dots.concat({
@@ -36,9 +52,11 @@ const addDot = () => {
 	rerender()
 }
 
-const el = render(width, height, dots)
+// -------------------------------
+
+const el = render(width, height, dots, removeDot, addDot)
 const rerender = () => {
-	yo.update(el, render(width, height, dots))
+	yo.update(el, render(width, height, dots, removeDot, addDot))
 }
 
 for (let i = 0; i < 10; i++) addDot()
